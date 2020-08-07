5cbaaaad14e2c5f0cb7a8bf752620c44383ddbfe
modares <modareshadi@gmail.com>
2020-08-07 10:15:56 -0700

    Done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,23 @@
-def has_negatives(a):
+def has_negatives(array):
     """
     YOUR CODE HERE
     """
     # Your code here
+    memory = dict()
+    # st()
 
+    for i in array:
+        i = abs(i)
+        try:
+            memory[i] += 1
+        except KeyError:
+            memory[i] = 1
+
+    result = [i[0] for i in memory.items() if i[1] > 1]
+
+    # st()
     return result
 
 
-if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+# if __name__ == "__main__":
+#     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
